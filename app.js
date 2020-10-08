56ff3a530877b2a1fc013811eb6038e0009fd058
Julia Lisec <julialisec12@gmail.com>
2020-10-08 11:54:46 +0200

    fixed navbar after scrolling more than navbar height is

--- app.js
+++ app.js
@@ -18,14 +18,28 @@ navToggle.addEventListener('click', function() {
   const linksHeight = links.getBoundingClientRect().height;
 
   if(containerHeight === 0) {
-    linksContainer.style.height = `${linksHeight}px`
+    linksContainer.style.height = `${linksHeight}px`;
   }
   else {
     linksContainer.style.height = 0;
   }
 });
 
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
+
 // ********** fixed navbar ************
+window.addEventListener('scroll', function() {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+
+  if(scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav');
+  }
+  else {
+    navbar.classList.remove('fixed-nav');
+  }
+});
 
 // ********** smooth scroll ************
 // select links
